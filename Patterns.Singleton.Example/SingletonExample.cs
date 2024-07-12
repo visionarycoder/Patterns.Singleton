4c97a3b674352cb194c52b1c9cf7e6a3ab87f3dd
Ivan Jones <ivanjo@vsp.com>
2024-07-12 09:32:09 -0700

    Initial Commit

---
+++ Patterns.Singleton.Example/SingletonExample.cs
@@ -0,0 +1,29 @@
+﻿// ReSharper disable InconsistentNaming
+namespace Patterns.Singleton.Example;
+
+public class SingletonExample
+{
+
+    private static readonly object padlock = default!;
+    private static SingletonExample? instance;
+
+    private SingletonExample()
+    {
+
+    }
+
+    public static SingletonExample Instance
+    {
+
+        get
+        {
+            lock (padlock)
+            {
+                instance ??= new SingletonExample();
+            }
+            return instance;
+        }
+
+    }
+
+}
